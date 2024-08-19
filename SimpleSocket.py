fa8997428d4a97d2117bfaa8dfe7e26497b7a71b
Aphain <nils-hagen@swabek.de>
2024-08-19 12:58:57 +0200

    new socket layout

--- SimpleSocket.py
+++ SimpleSocket.py
@@ -1,27 +1,72 @@
 import socket
+from collections.abc import Buffer
 
 from logger import log
 
 
-class SimpleSocket:
+class ServerSocket:
     # TODO: real IPvAddress type instead of str
-    def __init__(self, addr: (str, int), listen: bool = False):
+    def __init__(self, addr: (str, int)):
         self.addr = addr
 
-        if listen:
-            if socket.has_dualstack_ipv6():
-                self.socket = socket.create_server(addr, family=socket.AF_INET6, reuse_port=True, dualstack_ipv6=True)
-            else:
-                self.socket = socket.create_server(addr, family=socket.AF_INET, reuse_port=True)
-
-            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self.socket.bind((self.ip, self.port))
-            self.socket.listen(1)
+        # Should always be true on linux systems
+        if socket.has_dualstack_ipv6():
+            self.socket = socket.create_server(addr, family=socket.AF_INET6, backlog=1, reuse_port=True,
+                                               dualstack_ipv6=True)
         else:
+            self.socket = socket.create_server(addr, family=socket.AF_INET, backlog=1, reuse_port=True)
+
+        self.set_blocking(False)
+
+    def __del__(self):
+        self.close()
+
+    def set_blocking(self, setter: bool):
+        self.socket.setblocking(setter)
+
+    def close(self):
+        try:
+            self.socket.shutdown(socket.SHUT_RDWR)
+        except OSError:
+            pass
+
+        try:
+            self.socket.close()
+        except OSError:
+            pass
+
+        # log("Closed " + str(self.ip) + ":" + str(self.port))
+        self.addr = (" ", 0)
+
+    def accept(self):
+        try:
+            client_socket, client_address = self.socket.accept()
+
+            new_socket = ClientSocket(client_address, client_socket)
+
+            # ("Accepted new Client " + str(ip) + ":" + str(port))
+
+            return new_socket
+
+        except OSError:
+            # self.logger.log("Unable to accept")
+            return None
+
+
+class ClientSocket:
+    # TODO: real IPvAddress type instead of str
+    def __init__(self, addr: (str, int), other_socket=None):
+        self.addr = addr
+
+        if other_socket is None:
             try:
                 self.socket = socket.create_connection(self.addr, source_address=None)
-            except e:
+            except OSError:
                 pass
+        else:
+            self.socket = other_socket
+
+        self.set_blocking(False)
 
     def __del__(self):
         self.close()
@@ -33,17 +78,17 @@ class SimpleSocket:
         try:
             msg = self.socket.recv(4069)
         except OSError as e:
-            #log(str(e) + "on rec " + self.ip + ":" + str(self.port))
+            # log(str(e) + "on rec " + self.ip + ":" + str(self.port))
             return None
 
         return msg
 
-    def send(self, msg):
+    def send(self, msg: Buffer):
         try:
-            #log(str(self.ip) + ":" + str(self.port) + " <- " + str(msg))
+            # log(str(self.ip) + ":" + str(self.port) + " <- " + str(msg))
             return self.socket.send(msg)
         except OSError as e:
-            #log("fail " + str(e))
+            # log("fail " + str(e))
             return 0
 
     def close(self):
@@ -57,22 +102,5 @@ class SimpleSocket:
         except OSError:
             pass
 
-        #log("Closed " + str(self.ip) + ":" + str(self.port))
+        # log("Closed " + str(self.ip) + ":" + str(self.port))
         self.addr = (" ", 0)
-
-    def accept(self):
-        try:
-            clientSocket, clientAddress = self.socket.accept()
-
-            newSimpSock = SimpleSocket(clientSocket)
-
-            (ip, port) = clientAddress
-            newSimpSock.ip = ip
-            newSimpSock.port = port
-            #("Accepted new Client " + str(ip) + ":" + str(port))
-
-            return newSimpSock
-
-        except:
-            # self.logger.log("Unable to accept")
-            return None
