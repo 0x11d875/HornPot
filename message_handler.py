5be2ce276e734717d660e1aadd4eac9ff5c2420e
user <user@user>
2024-10-11 16:53:04 +0200

    fixed ignored urls

--- message_handler.py
+++ message_handler.py
@@ -2,7 +2,7 @@ import re
 import urllib.parse
 
 ignore_ip = ""
-ignored_urls = ["example.com", "etherx.jabber.org", "clientapi.ipip.net", "xmlsoap.org", "facebook.com", "fbcdn.net", "ytimg.com", "192.168.", "github.com", "inspici.com", "opensource.org"]
+ignored_urls = ["example.com", "etherx.jabber.org", "clientapi.ipip.net", "xmlsoap.org", "facebook.com", "fbcdn.net", "ytimg.com", "192.168.", "github.com", "inspici.com", "opensource.org", "google.com"]
 
 def find_urls(text):
 
